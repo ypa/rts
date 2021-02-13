5a8f1cf9edbb790f50bdc90a49164c4ba0992c1f
Yan P Aung <yan.pye.aung@gmail.com>
2021-02-12 19:40:18 -0800

    Add GuestList component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import ReactDOM from 'react-dom';
-
+import GuestList from './state/GuestList';
 
 const App = () => {
-  return <div>
-    <h1>Hi there!</h1>
-  </div>
+  return (
+    <div>
+      <GuestList />
+    </div>
+  );
 };
 
-
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
